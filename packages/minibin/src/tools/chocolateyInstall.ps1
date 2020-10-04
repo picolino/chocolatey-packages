b050a55c20a07b80bed5db2345d98d2907192eed
picolino <webmaster.pico@gmail.com>
2020-10-04 19:36:14 +0300

    Add minibin package sources and builds

---
+++ packages/minibin/src/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageName = 'minibin'
+$installerType = 'exe'
+$installerFileNameWithExtension = 'MiniBin-6.6.0.0-Setup.exe'
+$url = 'https://e-sushi.net/minibin/free/minibin.zip'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+$toolsDir = $(Split-Path -Parent $MyInvocation.MyCommand.Definition)
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$toolsDir"
+Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgs" "$toolsDir/$installerFileNameWithExtension" -validExitCodes $validExitCodes

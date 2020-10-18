f57bafa91bb444c48fdc902cd8b55c30b0932425
picolino <webmaster.pico@gmail.com>
2020-10-18 15:21:15 +0300

    Add sqlbench chocolatey package

---
+++ packages/sqlbench/1.1.0.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'minibin'
+$installerType = 'zip'
+$installerFileNameWithExtension = 'MiniBin-6.6.0.0-Setup.exe'
+$url = 'https://github.com/felixge/sqlbench/releases/download/v1.1.0/sqlbench_1.1.0_windows_amd64.zip'
+$checkSum = '0323409ee4c723cdbe292a8eacf92b907de5ddcd551318c825330aee1956e9fa'
+$toolsDir = $(Split-Path -Parent $MyInvocation.MyCommand.Definition)
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$toolsDir" -Checksum "$checkSum" -ChecksumType "sha256"

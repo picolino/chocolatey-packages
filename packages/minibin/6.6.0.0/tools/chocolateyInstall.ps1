9a25149ec7a1123770445af7b15b7482b8edf0b3
picolino <webmaster.pico@gmail.com>
2021-02-23 11:55:56 +0300

    Another fix for minibin >_>

--- packages/minibin/6.6.0.0/tools/chocolateyInstall.ps1
+++ packages/minibin/6.6.0.0/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'minibin'
 $installerType = 'exe'
 $installerFileNameWithExtension = 'MiniBin-6.6.0.0-Setup.exe'
-$url = 'https://e-sushi.net/sponsored-download/minibin.zip'
-$checkSum = '6AD238C3559B45C1EDF8781DA779A713602E8640C08649AF1A08F306255B9E8818132F83A3F003B801BF92D1599B57B388C2A95D3F416938D43ABAB5750574D2'
+$url = 'https://e-sushi.net/projects/minibin.zip'
+$checkSum = '6ad238c3559b45c1edf8781da779a713602e8640c08649af1a08f306255b9e8818132f83a3f003b801bf92d1599b57b388c2a95d3f416938d43abab5'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 $toolsDir = $(Split-Path -Parent $MyInvocation.MyCommand.Definition)

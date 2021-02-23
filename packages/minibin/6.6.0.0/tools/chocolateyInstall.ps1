8983951df21d40c4d37b8992ba55dd575909e608
picolino <webmaster.pico@gmail.com>
2021-02-23 12:01:38 +0300

    Fix checksum

--- packages/minibin/6.6.0.0/tools/chocolateyInstall.ps1
+++ packages/minibin/6.6.0.0/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $installerType = 'exe'
 $installerFileNameWithExtension = 'MiniBin-6.6.0.0-Setup.exe'
 $url = 'https://e-sushi.net/projects/minibin.zip'
-$checkSum = '6ad238c3559b45c1edf8781da779a713602e8640c08649af1a08f306255b9e8818132f83a3f003b801bf92d1599b57b388c2a95d3f416938d43abab5'
+$checkSum = '6ad238c3559b45c1edf8781da779a713602e8640c08649af1a08f306255b9e8818132f83a3f003b801bf92d1599b57b388c2a95d3f416938d43abab5750574d2'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 $toolsDir = $(Split-Path -Parent $MyInvocation.MyCommand.Definition)
